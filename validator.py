ba9c12f9cf9c1c02eda469917410d730c9c5cbec
Владислав <vladkorzun2016@yandex.ru>
2026-09-23 22:32:53 +0300

    fix: remove unnecessary

--- validator.py
+++ validator.py
@@ -12,6 +12,3 @@ def validate_email(email: str) -> bool:
     pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     return bool(re.match(pattern, email))
 
-def validate_inn(inn: str) -> bool:
-    """Функция валидации ИНН."""
-    return False  # Заглушка, пока не реализовано
